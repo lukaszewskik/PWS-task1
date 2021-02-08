045a3e061c4019926080a09abbdd9e1853b86075
lukaszewskik <51677513+lukaszewskik@users.noreply.github.com>
2021-02-08 20:07:04 +0200

    Commit 1

---
+++ Interactable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    PlayerInteraction playerInteraction;
+    readonly float maxDistanceToPlayer = 2;
+
+    private void Awake()
+    {
+        playerInteraction = FindObjectOfType<PlayerInteraction>();
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interaction");
+    }
+
+    public virtual bool CanInteract()
+    {
+        float distance;
+        distance = Vector3.Distance(transform.position, playerInteraction.transform.position);
+
+        if (distance <= maxDistanceToPlayer)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInteraction.SubscribeInteractable(this);
+        }      
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInteraction.UnsubscribeInteractable();
+        }
+    }
+}
---
+++ PlayerInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    
+    Interactable objectToInteract;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && objectToInteract)
+        {
+            InteractWithInteractable();
+        }
+    }
+
+    void InteractWithInteractable()
+    {
+        if (objectToInteract.CanInteract())
+        {
+            objectToInteract.Interact();
+        }
+        
+    }
+
+    public void SubscribeInteractable(Interactable obj)
+    {
+        objectToInteract = obj;
+    }
+
+    public void UnsubscribeInteractable()
+    {
+        objectToInteract = null;
+    }
+
+}
